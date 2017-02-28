f53f0ea47252cd9ec3f26a836352c93804b1d296
David <46406163y@iespoblenou.org>
2017-02-28 16:50:50 +0100

    Initial commit, mas fragment_main

---
+++ app/src/main/java/com/example/a46406163y/proyectouf2/MainActivityFragment.java
@@ -0,0 +1,41 @@
+package com.example.a46406163y.proyectouf2;
+
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return view;
+
+    }
+
+}
